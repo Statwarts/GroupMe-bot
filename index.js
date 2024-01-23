937c315453212acd4257b1cb03aa5f8ae185069a
Divyam Sharma <divyam.7379@gmail.com>
2024-01-23 22:55:24 +0530

    commented Firebase

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const StoreMessage = require("./Firebase/StoreMessage.cjs");
+// const StoreMessage = require("./Firebase/StoreMessage.cjs");
 
 const express = require("express");
 const axios = require("axios");
@@ -28,7 +28,7 @@ app.post("/receive", async (req, res) => {
     sendMessage(`Goodbye! ${name}`);
   }
   console.log(req.body.text, " by ", req.body.name);
-  StoreMessage(req.body.text);
+  // StoreMessage(req.body.text);
 });
 
 // function sendMessage(text) {
