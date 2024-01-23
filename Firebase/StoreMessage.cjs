794f5fdccf53656d2d3f629bc4afb3727a52d470
Divyam Sharma <divyam.7379@gmail.com>
2024-01-23 23:06:49 +0530

    testing firebase

--- Firebase/StoreMessage.cjs
+++ Firebase/StoreMessage.cjs
@@ -1,18 +1,18 @@
 // import { db, storage, app } from './FirebaseConfig.mjs';
-// (async () => {
-//   const { db, storage, app } = await import("./FirebaseConfig.mjs");
+(async () => {
+  const { db, storage, app } = await import("./FirebaseConfig.mjs");
 
-//   const StoreMessage = async (message) => {
-//     try {
-//       const docRef = await addDoc(collection(db, "messages"), {
-//         message: message,
-//         timestamp: serverTimestamp(),
-//       });
-//       console.log("Document written with ID: ", docRef.id);
-//     } catch (e) {
-//       console.error("Error adding document: ", e);
-//     }
-//   };
-//   module.exports = StoreMessage;
-// })();
+  const StoreMessage = async (message) => {
+    try {
+      const docRef = await addDoc(collection(db, "messages"), {
+        message: message,
+        timestamp: serverTimestamp(),
+      });
+      console.log("Document written with ID: ", docRef.id);
+    } catch (e) {
+      console.error("Error adding document: ", e);
+    }
+  };
+  module.exports = StoreMessage;
+})();
 // export default StoreMessage;
