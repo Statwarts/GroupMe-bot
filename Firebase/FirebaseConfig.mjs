83b835a11e52cfd534b2d6173c2e51cdafe5d08b
Divyam Sharma <divyam.7379@gmail.com>
2024-01-23 23:09:43 +0530

    visiblity update

--- Firebase/FirebaseConfig.mjs
+++ Firebase/FirebaseConfig.mjs
@@ -1,28 +1,28 @@
 
-import { initializeApp } from "firebase/app";
-// import { getAnalytics } from "firebase/analytics";
-import { getStorage } from "firebase/storage";
-import { getFirestore } from "firebase/firestore";
+// import { initializeApp } from "firebase/app";
+// // import { getAnalytics } from "firebase/analytics";
+// import { getStorage } from "firebase/storage";
+// import { getFirestore } from "firebase/firestore";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDkjfslq4x_cSyGs23KTrhR_ZAhWhlWEQI",
-  authDomain: "groupme-bot-b8ffd.firebaseapp.com",
-  projectId: "groupme-bot-b8ffd",
-  storageBucket: "groupme-bot-b8ffd.appspot.com",
-  messagingSenderId: "155140118986",
-  appId: "1:155140118986:web:37ba0367083807e2bd9733",
-  measurementId: "G-T2DNS8SXFP"
-};
+// const firebaseConfig = {
+//   apiKey: "AIzaSyDkjfslq4x_cSyGs23KTrhR_ZAhWhlWEQI",
+//   authDomain: "groupme-bot-b8ffd.firebaseapp.com",
+//   projectId: "groupme-bot-b8ffd",
+//   storageBucket: "groupme-bot-b8ffd.appspot.com",
+//   messagingSenderId: "155140118986",
+//   appId: "1:155140118986:web:37ba0367083807e2bd9733",
+//   measurementId: "G-T2DNS8SXFP"
+// };
 
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-const db = getFirestore(app);
-const storage = getStorage(app);
-// const analytics = getAnalytics(app);
+// // Initialize Firebase
+// const app = initializeApp(firebaseConfig);
+// const db = getFirestore(app);
+// const storage = getStorage(app);
+// // const analytics = getAnalytics(app);
 
 
-// export {db , storage , app};
+// // export {db , storage , app};
 
-// export default app;
+// // export default app;
 
-export default {db , storage , app};
+// export default {db , storage , app};
