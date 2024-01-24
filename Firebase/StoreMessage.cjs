bedc731c157cbf2b77276fff35165aec56a2f9f2
Divyam Sharma <divyam.7379@gmail.com>
2024-01-24 18:08:41 +0530

    firebase doc fix

--- Firebase/StoreMessage.cjs
+++ Firebase/StoreMessage.cjs
@@ -2,9 +2,11 @@
 const { db, storage, app } = require('./FirebaseConfig.js');
 const { addDoc, collection, serverTimestamp } = require('firebase/firestore');
 const StoreMessage = async (message) => {
+  const {text,name} = message;
   try {
     const docRef = await addDoc(collection(db, "messages"), {
-      message: message,
+      message: text,
+      sender: name,
       timestamp: serverTimestamp(),
     });
     console.log("Document written with ID: ", docRef.id);
