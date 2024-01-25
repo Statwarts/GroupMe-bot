333668114f8110dfaa803a6e818c065602a6eda0
Divyam Sharma <divyam.7379@gmail.com>
2024-01-25 23:28:47 +0530

    summary bug fix

--- summariser.js
+++ summariser.js
@@ -1,26 +1,29 @@
 const axios = require("axios");
 
 const Summarize = async (textToSummarize) => {
-  const options = {
-    method: "POST",
-    url: "https://text-analysis12.p.rapidapi.com/language-detection/api/v1.1",
-    headers: {
-      "content-type": "application/json",
-      "X-RapidAPI-Key": process.env.RapidAPIKEY,
-      "X-RapidAPI-Host": "text-analysis12.p.rapidapi.com",
-    },
-    data: {
-      text: textToSummarize,
-    },
-  };
+    const axios = require('axios');
 
-  try {
-    const response = await axios.request(options);
-    return response.data;
-    console.log(response.data);
-  } catch (error) {
-    console.error(error);
-  }
+    const options = {
+      method: 'POST',
+      url: 'https://text-analysis12.p.rapidapi.com/summarize-text/api/v1.1',
+      headers: {
+        'content-type': 'application/json',
+        'X-RapidAPI-Key': process.env.RapidAPIKey,
+        'X-RapidAPI-Host': 'text-analysis12.p.rapidapi.com'
+      },
+      data: {
+        language: 'english',
+        summary_percent: 10,
+        text: textToSummarize
+      }
+    };
+    
+    try {
+        const response = await axios.request(options);
+        console.log(response.data);
+    } catch (error) {
+        console.error(error);
+    }
 };
 
 module.exports = Summarize;
