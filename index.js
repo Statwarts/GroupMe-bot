8c1790d632a7612fdcf5af857e139939b779669d
Divyam Sharma <divyam.7379@gmail.com>
2024-01-26 00:12:23 +0530

    summary bug fix v3

--- index.js
+++ index.js
@@ -152,7 +152,7 @@ async function sendMessage(text) {
 }
 
 app.listen(3000, () => {
-  console.log(typeof process.env.RAPIDAPI_KEY);
+  // console.log(typeof process.env.RAPIDAPI_KEY);
   console.log("Listening on port 3000");
 });
 
--- summariser.js
+++ summariser.js
@@ -6,26 +6,26 @@ const min = (a,b) => {
 
 const Summarize = async (textToSummarize,summary_percent) => {
     const axios = require('axios');
-    console.log(textToSummarize);
+    // console.log(textToSummarize);
     const options = {
       method: 'POST',
       url: 'https://text-analysis12.p.rapidapi.com/summarize-text/api/v1.1',
       headers: {
         'content-type': 'application/json',
-        'X-RapidAPI-Key': process.env.RAPIDAPI_KEY,
+        'X-RapidAPI-Key': '15b80fdf3emsh5acd32eeb76f5e6p1a0c6ajsn3f1562d9701a',
         'X-RapidAPI-Host': 'text-analysis12.p.rapidapi.com'
       },
       data: {
         language: 'english',
-        summary_percent: min(50,summary_percent),
+        summary_percent: 30,
         text: textToSummarize
       }
     };
     
     try {
         const response = await axios.request(options);
-        return response.data.summary;
         console.log(response.data);
+        return response.data.summary;
     } catch (error) {
         console.error(error);
     }
