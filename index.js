9cfed2b2c29f892b6708a79e41dc04d688823261
Divyam Sharma <divyam.7379@gmail.com>
2024-01-25 14:40:27 +0530

    bug fix

--- index.js
+++ index.js
@@ -24,6 +24,9 @@ app.post("/receive", async (req, res) => {
   if (req.body.sender_type === "bot") {
     return;
   }
+  if(command[0] !== '/'){
+    StoreMessage(req.body);
+  }
   switch (system) {
     case true:
       if (text.includes("has joined the group")) {
@@ -117,38 +120,7 @@ app.post("/receive", async (req, res) => {
   }
 });
 
-//   if (req.body.text === "/ping") {
-//     sendMessage("Pong!");
-//     console.log("Ponged!");
-//   }
-//   if (
-//     req.body.text.includes("has joined the group") &&
-//     req.body.system === true
-//   ) {
-//     const name = req.body.text.split(" has joined the group")[0];
-//     sendMessage(`Welcome to the group! ${name}`);
-//   }
-//   if (
-//     req.body.text.includes("has left the group") &&
-//     req.body.system === true
-//   ) {
-//     const name = req.body.text.split(" has left the group")[0];
-//     sendMessage(`Goodbye! ${name}`);
-//   }
-//   console.log(req.body.text, " by ", req.body.name);
-//   StoreMessage(req.body);
-// });
 
-// function sendMessage(text) {
-//   axios.post("https://api.groupme.com/v3/bots/post", {
-//     method: "post",
-//     Headers: {
-//       "Content-Type": "application/json",
-//     },
-//     bot_id: process.env.BOT_TOKEN,
-//     text: text,
-//   });
-// }
 async function sendMessage(text) {
   try {
     await axios.post(
